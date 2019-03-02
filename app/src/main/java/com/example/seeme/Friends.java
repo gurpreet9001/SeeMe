22660c8369fd1f18afcac7027dfc461fb58bd04f
gurpreet9001 <gpelia001@gmail.com>
2019-03-02 08:48:33 +0530

    show friends in tab3

---
+++ app/src/main/java/com/example/seeme/Friends.java
@@ -0,0 +1,25 @@
+package com.example.seeme;
+
+public class Friends  {
+
+    public Friends(String date) {
+        this.date = date;
+    }
+
+    public Friends(){
+
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    String date;
+
+
+
+}
